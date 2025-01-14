51839b279958c8528b4e6dd7a4aa4c1516aebb41
Atmaram Naik <naik.atmaram@gmail.com>
2025-01-14 18:27:11 +0530

    login feature complete

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,12 @@
-export default function HomePage(){
-    return <div>Hello User</div>
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation";
+
+export default async function HomePage(){
+    const store = await cookies()
+      const session = store.get('session')
+      if (!session){
+        redirect("/login")
+      } else {
+        return <div>Home Page</div>
+      }
 }
