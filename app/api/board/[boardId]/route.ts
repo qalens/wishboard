1c8d95abbee87291c58f80abb1c7a055de7581e5
Atmaram Naik <naik.atmaram@gmail.com>
2025-01-15 21:02:41 +0530

    fix type issues

--- app/api/board/[boardId]/route.ts
+++ app/api/board/[boardId]/route.ts
@@ -2,15 +2,15 @@ import { getBoard, updateBoard } from "@/app/db/board"
 import { getUserFromSession } from "@/app/lib/session"
 import { NextResponse } from "next/server"
 
-export async function PATCH(req: Request,{ params }: { params: { boardId: string } }) {
+export async function PATCH(req: Request,{ params }: { params: Promise<{ boardId: string }> }) {
     const { title, validTill } = await req.json()
-    const {boardId} = params
+    const {boardId} = await params
     const payload = await getUserFromSession()
     const result = await updateBoard(boardId,payload.userId as string,title, validTill)
     return NextResponse.json({ message: "board updated", data: result }, { status: 200 })
 }
-export async function GET(req: Request,{ params }: { params: { boardId: string } }) {
-    const {boardId} = params
+export async function GET(req: Request,{ params }: { params: Promise<{ boardId: string }> }) {
+    const {boardId} = await params
     try{
         const result = await getBoard(boardId)
         return NextResponse.json({ message: "got board", data: result }, { status: 200 })
--- app/board/[id]/page.tsx
+++ app/board/[id]/page.tsx
@@ -1,5 +1,10 @@
 import Board from "@/components/board/board";
 
-export default function IndividualBoard({params:{id}}:{params:{id:string}}){
+export default async function Page({
+    params,
+  }: {
+    params: Promise<{ id: string }>
+  }) {
+    const {id}=await params
     return <Board id={id}/>
 }
